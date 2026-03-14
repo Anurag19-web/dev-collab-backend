6c4a3c7b285235d08dd0cfdfef0fe9796fb25b91
Anurag19-web <anuragnayak78910@gmail.com>
2026-03-14 21:47:13 +0530

    jwt removed

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,5 @@
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
 import generateUserId from "../utils/generateUserId.js";
 
 export const signup = async (req, res) => {
